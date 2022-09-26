94ccd5990195dd6434005ac1eda7a0180efb79a1
albrecht <albrecht.michael@hotmail.fr>
2022-09-26 20:33:31 +0200

    config environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
 import { SOURCES } from 'config/sources';
 
+enum mongoURL {
+  mongoLocal = 'http://localhost:5500',
+  mongoVercel = 'https://missions-to-get-pokemons-backend-wxop.vercel.app',
+}
 export const environment = {
   production: false,
-  source: SOURCES.restPokeApi,
+  pokemonSource: SOURCES.restPokeApi,
+  pokemonCaughtSource: SOURCES.mongo,
+  pokemonTypesSource: SOURCES.inmemory,
+  missionSource: SOURCES.mongo,
+
+  mongoURL: mongoURL.mongoVercel,
 };
