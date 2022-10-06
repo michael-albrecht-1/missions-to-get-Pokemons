431315116530fc01b14fe8b13f1ec3e82ff64d0d
= <michael.albrecht@odeis.net>
2022-10-06 23:43:10 +0200

    remove import

--- src/app/mission/adapters/secondaries/real/mongoMission.loader.ts
+++ src/app/mission/adapters/secondaries/real/mongoMission.loader.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Mission } from 'src/app/mission/domain/entity/mission';
 import { environment } from 'src/environments/environment';
-import { MissionSnapshot } from '../../../domain/entity/mission.snapshot';
 import { MissionLoader } from '../../../usecases/loaders/mission.loader';
 import { MongoMissionMapper } from './mission.mapper';
 import { MongoMissionDTO } from './mongoMission.DTO';
