3354d138f3b846068e4ce8735a646b96814ba5ec
michael.albrecht <albrecht.michael@hotmail.com>
2022-10-24 22:41:27 +0200

    refact pokemon listing observables

--- src/app/pokemon/adapters/primaries/pokemon-listing/pokemon-listing.component.ts
+++ src/app/pokemon/adapters/primaries/pokemon-listing/pokemon-listing.component.ts
@@ -1,6 +1,14 @@
 import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { first, map, switchMap, tap } from 'rxjs';
+import {
+  combineLatest,
+  first,
+  map,
+  startWith,
+  Subject,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import { CaughtPokemon } from 'src/app/caughtPokemon/domain/entity/caughtPokemon';
 import { IGetCaughtPokemons } from 'src/app/caughtPokemon/usecases/IGetCaughtPokemons';
 import { Pokemon } from '../../../domain/entity/pokemon';
@@ -20,7 +28,9 @@ export class PokemonListingComponent {
   public form: FormGroup = this.#initForm();
 
   public pokemons: Pokemon[] = [];
-  public fiteredTypeCaughtPokemons!: number;
+  public fiteredTypeCaughtPokemonsCount!: number;
+
+  #destroy$ = new Subject<void>();
 
   constructor(
     @Inject('ISearchAllPokemons')
@@ -31,42 +41,25 @@ export class PokemonListingComponent {
   ) {}
 
   ngOnInit(): void {
-    this.form.valueChanges
+    const formChanges$ = this.form.valueChanges.pipe(
+      startWith(this.form.value)
+    );
+    const allPokemons$ = this.#getAllPokemons$();
+    const pokemonsCaught$ = this.#getPokemonCaught$();
+
+    combineLatest([formChanges$, allPokemons$, pokemonsCaught$])
       .pipe(
-        map((formValue) => {
-          const filtredPokemonsByTypes = this.#filterPokemonsByType(
-            this.pokemons,
-            formValue.pokemonType
-          );
-
-          const filtredPokemonsByTypesAndCaught = this.#filterPokemonsByCaught(
-            filtredPokemonsByTypes,
-            formValue.filterCaughtPokemons
-          );
-
-          return (this.filteredPokemons = filtredPokemonsByTypesAndCaught);
-        }),
-        map((filteredPokemons: Pokemon[]) => {
-          return (this.fiteredTypeCaughtPokemons = filteredPokemons.filter(
-            (pokemon: Pokemon) => {
-              const caughtPokemon = this.caughtPokemons.find(
-                (caughtPokemon: CaughtPokemon) =>
-                  caughtPokemon.snapshot().number === pokemon.snapshot().number
-              );
-              return caughtPokemon ? true : false;
-            }
-          ).length);
-        })
+        map(() => this.form.value),
+        map(this.#setFiltredPokemons),
+        map(this.#setFiltredTypeCaughtPokemonsCount),
+        takeUntil(this.#destroy$)
       )
       .subscribe();
+  }
 
-    this.#getPokemonCaught$()
-      .pipe(
-        switchMap(this.#getAllPokemons$),
-        tap(() => this.form.controls['filterCaughtPokemons'].setValue(true))
-      )
-
-      .subscribe();
+  ngOnDestroy() {
+    this.#destroy$.next();
+    this.#destroy$.complete();
   }
 
   onAddPokemon(event: any) {
@@ -74,28 +67,42 @@ export class PokemonListingComponent {
   }
 
   #getPokemonCaught$ = () => {
-    return this.iGetCaughtPokemons.execute().pipe(
-      first(),
-      map(
-        (caughtPokemons: CaughtPokemon[]) =>
-          (this.caughtPokemons = caughtPokemons)
-      )
-    );
+    return this.iGetCaughtPokemons
+      .execute()
+      .pipe(
+        map(
+          (caughtPokemons: CaughtPokemon[]) =>
+            (this.caughtPokemons = caughtPokemons)
+        )
+      );
   };
 
   #getAllPokemons$ = () => {
-    return this.iSearchAllPokemons.execute().pipe(
-      first(),
-      map((pokemons: Pokemon[]) => (this.pokemons = pokemons))
-    );
+    return this.iSearchAllPokemons
+      .execute()
+      .pipe(map((pokemons: Pokemon[]) => (this.pokemons = pokemons)));
   };
 
-  #initForm() {
+  #initForm(): FormGroup<any> {
     return this.fb.group({
       pokemonType: [],
       filterCaughtPokemons: [true, { nonNullable: true }],
     });
   }
+
+  #setFiltredPokemons = (formValue: any): Pokemon[] => {
+    const filtredPokemonsByTypes = this.#filterPokemonsByType(
+      this.pokemons,
+      formValue.pokemonType
+    );
+
+    const filtredPokemonsByTypesAndCaught = this.#filterPokemonsByCaught(
+      filtredPokemonsByTypes,
+      formValue.filterCaughtPokemons
+    );
+
+    return (this.filteredPokemons = filtredPokemonsByTypesAndCaught);
+  };
 
   #filterPokemonsByCaught(
     pokemons: Pokemon[],
@@ -129,5 +136,19 @@ export class PokemonListingComponent {
     return pokemons.filter((pokemon: Pokemon): boolean =>
       pokemon.hasType(pokemonType)
     );
+  };
+
+  #setFiltredTypeCaughtPokemonsCount = (
+    filteredPokemons: Pokemon[]
+  ): number => {
+    return (this.fiteredTypeCaughtPokemonsCount = filteredPokemons.filter(
+      (pokemon: Pokemon) => {
+        const caughtPokemon = this.caughtPokemons.find(
+          (caughtPokemon: CaughtPokemon) =>
+            caughtPokemon.snapshot().number === pokemon.snapshot().number
+        );
+        return caughtPokemon ? true : false;
+      }
+    ).length);
   };
 }
